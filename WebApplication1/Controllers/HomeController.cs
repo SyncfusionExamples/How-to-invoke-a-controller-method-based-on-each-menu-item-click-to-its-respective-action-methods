90c8b0cbbe61a74ad84a0e704fc05bfb6ead7492
Manikandan-Lakshmanan <93248283+Manikandan-Lakshmanan@users.noreply.github.com>
2022-04-22 12:30:33 +0530

    documentation(EJ2-18942-menu): Sample Added

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        
+        public IActionResult Open()
+    {
+            return View(); // Triggered for Save click
+        }
+        public IActionResult Save()
+    {
+            return View(); // Triggered for Save click
+        }
+        public IActionResult Exit()
+    {
+            return View();   // Triggered for Exit Click
+        }
+    }
+}
