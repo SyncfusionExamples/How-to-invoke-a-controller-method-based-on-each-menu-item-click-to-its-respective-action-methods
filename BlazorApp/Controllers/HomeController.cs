92d9392803cbe2eb4c55786ae90ac7ce82061873
Manikandan-Lakshmanan <93248283+Manikandan-Lakshmanan@users.noreply.github.com>
2022-06-03 17:22:26 +0530

    documentation(BLAZ-18942): Sample Added

---
+++ BlazorApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+       public void Save()
+        {
+          Console.WriteLine("save is clicked");
+        }
+    }
+}
---
+++ CoreWebApp/CoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoreWebApp.Controllers
+{
+    
+    
+        public class HomeController : Controller
+        {
+
+            public IActionResult Index()
+            {
+
+                return View();
+            }
+
+            public IActionResult Open()
+            {
+                return View(); // Triggered for Open click
+            }
+            public IActionResult Save()
+            {
+                return View(); // Triggered for Save click
+            }
+            public IActionResult Exit()
+            {
+                return View();   // Triggered for Exit Click
+            }
+        }
+    
+}
